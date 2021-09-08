73abb9b5a7151a0719a52042bc9fadde934fc2af
Phonekham <phone.madridista@gmail.com>
2021-09-08 15:20:57 +0700

    firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD1eb0N8yJ8GJz-u8cqUtUyP8XVdhpZy2o",
+  authDomain: "mern-ecommerce-7c169.firebaseapp.com",
+  projectId: "mern-ecommerce-7c169",
+  storageBucket: "mern-ecommerce-7c169.appspot.com",
+  messagingSenderId: "546124281266",
+  appId: "1:546124281266:web:62e0ef33fe46001bd120ab",
+  measurementId: "G-Z6VWWFSKVR",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// export
+export const auth = firebase.auth();
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
