4503906ee9bd7ea11a7d0a495019bc7b7ea49980
Phonekham <phone.madridista@gmail.com>
2021-09-08 15:23:49 +0700

    register form

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,5 +1,38 @@
+import { useState } from "react";
+
 const Register = () => {
-  return <div>Register</div>;
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = () => {
+    //
+  };
+
+  const registerForm = () => (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        className="form-control"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        autoFocus
+      />
+
+      <button type="submit" className="btn btn-raised">
+        Register
+      </button>
+    </form>
+  );
+
+  return (
+    <div className="container p-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <h4>Register</h4>
+          {registerForm()}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
