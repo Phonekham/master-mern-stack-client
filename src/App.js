56ca90f533d9c45e43291738b78543c8c8019a2d
Phonekham <phone.madridista@gmail.com>
2021-09-09 11:30:19 +0700

    setup redux & set user to redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Switch, Route } from "react-router-dom";
 
 import Header from "./components/nav/Header";
+import { auth } from "./firebase";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import RegisterComplete from "./pages/auth/RegisterComplete";
 import Home from "./pages/Home";
 
 function App() {
+  const dispatch = useDispatch();
+
+  // To check firebase auth state
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const idTokenResult = await user.getIdTokenResult();
+
+        dispatch({
+          type: "LOGGED_IN_USER",
+          payload: {
+            email: user.email,
+            token: idTokenResult,
+          },
+        });
+      }
+    });
+    // Cleanup
+    return () => unsubscribe();
+  }, []);
+
   return (
     <>
       <Header />
--- src/index.js
+++ src/index.js
@@ -3,19 +3,27 @@ import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+// Redux
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers";
 
 import App from "./App";
 import "antd/dist/antd.css";
 import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
 
+// store
+const store = createStore(rootReducer, composeWithDevTools());
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
       <ToastContainer />
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
 
