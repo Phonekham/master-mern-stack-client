776a5c2224daaacc1e2b55353859fed81114455c
Phonekham <phone.madridista@gmail.com>
2021-09-16 15:00:59 +0700

    star rating

--- src/components/cards/SingleProduct.jsx
+++ src/components/cards/SingleProduct.jsx
@@ -1,16 +1,19 @@
 import { Card, Tabs } from "antd";
 import { Link } from "react-router-dom";
 import { HeartOutlined, ShoppingCartOutlined } from "@ant-design/icons";
+import StarRating from "react-star-ratings";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 import Laptop from "../../images/laptop.png";
 import ProductListItems from "./ProductListItems";
+import RatingModal from "../modal/RatingModal";
+import { showAverage } from "../../functions/rating";
 
 const { TabPane } = Tabs;
 
-const SingleProduct = ({ product }) => {
-  const { title, description, images, slug } = product;
+const SingleProduct = ({ product, onStarClick, star }) => {
+  const { title, description, images, _id } = product;
 
   return (
     <>
@@ -34,6 +37,11 @@ const SingleProduct = ({ product }) => {
 
       <div className="col-md-5">
         <h1 className="bg-info p-3">{title}</h1>
+        {product && product.ratings && product.ratings.length > 0 ? (
+          showAverage(product)
+        ) : (
+          <div className="text-center pt-1 pb-3">No rating yet</div>
+        )}
         <Card
           actions={[
             <>
@@ -43,6 +51,16 @@ const SingleProduct = ({ product }) => {
             <Link to="/">
               <HeartOutlined className="text-info" /> <br /> Add to Wishlist
             </Link>,
+            <RatingModal>
+              <StarRating
+                name={_id}
+                numberOfStars={5}
+                rating={star}
+                changeRating={onStarClick}
+                isSelectable={true}
+                starRatedColor="red"
+              />
+            </RatingModal>,
           ]}
         >
           <ProductListItems product={product} />
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,24 +1,47 @@
 import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 import SingleProduct from "../components/cards/SingleProduct";
-import { getProduct } from "../functions/product";
+import { getProduct, productStar } from "../functions/product";
 
 const Product = ({ match }) => {
   const [product, setProduct] = useState({});
+  const [star, setStar] = useState(0);
 
+  const { user } = useSelector((state) => ({ ...state }));
   const { slug } = match.params;
 
   useEffect(() => {
     loadSingleProduct();
   }, [slug]);
+  useEffect(() => {
+    if (product.ratings && user) {
+      let existingRatingObject = product.ratings.find(
+        (ele) => ele.postedBy.toString() === user._id.toString()
+      );
+      existingRatingObject && setStar(existingRatingObject.star); // current user's star
+    }
+  });
 
   const loadSingleProduct = () =>
     getProduct(slug).then((res) => setProduct(res.data));
 
+  const onStarClick = (newRating, name) => {
+    setStar(newRating);
+    productStar(name, newRating, user.token).then((res) => {
+      console.log("rating clicked", res.data);
+      loadSingleProduct(); // if you want to show updated rating in real time
+    });
+  };
+
   return (
     <div className="container-fluid">
       <div className="row pt-4">
-        <SingleProduct product={product} />
+        <SingleProduct
+          product={product}
+          onStarClick={onStarClick}
+          star={star}
+        />
       </div>
 
       <div className="row">
