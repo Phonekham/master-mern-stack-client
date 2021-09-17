6c7a8b1424a9ed84ea28e8f6732afa21118c7d84
Phonekham <phone.madridista@gmail.com>
2021-09-17 11:44:15 +0700

    filter by category

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { Menu, Slider } from "antd";
-import { DollarOutlined } from "@ant-design/icons";
+import { Menu, Slider, Checkbox } from "antd";
+import { DollarOutlined, DownSquareOutlined } from "@ant-design/icons";
 
 import ProductCard from "../components/cards/ProductCard";
+import { getCategories } from "../functions/category";
 import {
   fetchProductsByFilter,
   getProductsByCount,
@@ -15,6 +16,8 @@ const Shop = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [price, setPrice] = useState([0, 0]);
+  const [categories, setCategories] = useState([]);
+  const [categoryIds, setCategoryIds] = useState([]);
   const [ok, setOk] = useState(false);
 
   let dispatch = useDispatch();
@@ -23,7 +26,15 @@ const Shop = () => {
 
   useEffect(() => {
     loadAllProducts();
+    // fetch categories
+    getCategories().then((res) => setCategories(res.data));
   }, []);
+
+  const fetchProducts = (arg) => {
+    fetchProductsByFilter(arg).then((res) => {
+      setProducts(res.data);
+    });
+  };
 
   // 1. load products by default on page load
   const loadAllProducts = () => {
@@ -46,11 +57,23 @@ const Shop = () => {
     fetchProducts({ price });
   }, [ok]);
 
-  const fetchProducts = (arg) => {
-    fetchProductsByFilter(arg).then((res) => {
-      setProducts(res.data);
-    });
-  };
+  // 4. load products based on category
+  // show categories in a list of checkbox
+  const showCategories = () =>
+    categories.map((c) => (
+      <div key={c._id}>
+        <Checkbox
+          onChange={handleCheck}
+          className="pb-2 pl-4 pr-4"
+          value={c._id}
+          name="category"
+          checked={categoryIds.includes(c._id)}
+        >
+          {c.name}
+        </Checkbox>
+        <br />
+      </div>
+    ));
 
   const handleSlider = (value) => {
     console.log(value);
@@ -62,6 +85,30 @@ const Shop = () => {
     setTimeout(() => {
       setOk(!ok);
     }, 300);
+  };
+
+  const handleCheck = (e) => {
+    dispatch({
+      type: "SEARCH_QUERY",
+      payload: { text: "" },
+    });
+    setPrice([0, 0]);
+    // console.log(e.target.value);
+    let inTheState = [...categoryIds];
+    let justChecked = e.target.value;
+    let foundInTheState = inTheState.indexOf(justChecked); // index or -1
+
+    // indexOf method ?? if not found returns -1 else return index [1,2,3,4,5]
+    if (foundInTheState === -1) {
+      inTheState.push(justChecked);
+    } else {
+      // if found pull out one item from index
+      inTheState.splice(foundInTheState, 1);
+    }
+
+    setCategoryIds(inTheState);
+    console.log(inTheState);
+    fetchProducts({ category: inTheState });
   };
 
   return (
@@ -89,6 +136,17 @@ const Shop = () => {
                   max="4999"
                 />
               </div>
+            </SubMenu>
+            {/* category */}
+            <SubMenu
+              key="2"
+              title={
+                <span className="h6">
+                  <DownSquareOutlined /> Categories
+                </span>
+              }
+            >
+              <div style={{ maringTop: "-10px" }}>{showCategories()}</div>
             </SubMenu>
           </Menu>
         </div>
