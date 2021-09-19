5e6469fb8ea49489fb28aadb16c2c8cad0b4dec5
Phonekham <phone.madridista@gmail.com>
2021-09-19 11:28:24 +0700

    admin orders

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -1,6 +1,32 @@
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+
 import AdminNav from "../../components/nav/AdminNav";
+import Orders from "../../components/order/Orders";
+import { getOrders, changeStatus } from "../../functions/admin";
 
 const AdminDashboard = () => {
+  const [orders, setOrders] = useState([]);
+  const { user } = useSelector((state) => ({ ...state }));
+
+  useEffect(() => {
+    loadOrders();
+  }, []);
+
+  const loadOrders = () =>
+    getOrders(user.token).then((res) => {
+      console.log(JSON.stringify(res.data, null, 4));
+      setOrders(res.data);
+    });
+
+  const handleStatusChange = (orderId, orderStatus) => {
+    changeStatus(orderId, orderStatus, user.token).then((res) => {
+      toast.success("Status updated");
+      loadOrders();
+    });
+  };
+
   return (
     <div className="container-fluid">
       <div className="row">
@@ -10,6 +36,7 @@ const AdminDashboard = () => {
 
         <div className="col">
           <h4>Admin Dashboard</h4>
+          <Orders orders={orders} handleStatusChange={handleStatusChange} />
         </div>
       </div>
     </div>
